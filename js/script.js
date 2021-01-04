8918e7895e91f22a44b13b6bceb910e454b2c022
annafaya <annaluizafaya@gmail.com>
2021-01-04 19:31:10 +0100

    letss

--- js/script.js
+++ js/script.js
@@ -1,32 +1,96 @@
-$('.carousel').carousel({
-    interval: 3000,
-  })
+/*navbar*/
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 10) {
+      $(".navbar").addClass("active");
+    } else {
+      $(".navbar").removeClass("active");
+    }
+  });
+});
+/*end navbar*/
 
-  $(document).ready(function() {
-	
-    $('.card').delay(1800).queue(function(next) {
-      $(this).removeClass('hover');
-      $('a.hover').removeClass('hover');
+/* home carrousel*/
+$(".carousel").carousel({
+  interval: 3000,
+});
+
+$(document).ready(function () {
+  $(".card")
+    .delay(1800)
+    .queue(function (next) {
+      $(this).removeClass("hover");
+      $("a.hover").removeClass("hover");
       next();
     });
-  });
+});
+/*end home carrousel*/
 
-  function readMore(pessoa) {
-    let dots = document.querySelector(`.card[data-pessoa="${pessoa}"] .dots`);
-    let moreText = document.querySelector(
-      `.card[data-pessoa="${pessoa}"] .more`
-    );
-    let btnText = document.querySelector(
-      `.card[data-pessoa="${pessoa}"] .myBtn`
-    );
+/*read more produtora.html*/
+function readMore(pessoa) {
+  let dots = document.querySelector(`.item[data-pessoa="${pessoa}"] .dots`);
+  let moreText = document.querySelector(`.item[data-pessoa="${pessoa}"] .more`);
+  let btnText = document.querySelector(`.item[data-pessoa="${pessoa}"] .myBtn`);
 
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.textContent = "Saiba mais...";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.textContent = "Leia menos";
-      moreText.style.display = "inline";
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.textContent = "Saiba mais...";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.textContent = "menos";
+    moreText.style.display = "inline";
+  }
+}
+/* end read more produtora.html*/
+
+/*filter producoes.html*/
+filterSelection("all");
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
     }
   }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+/*END filter producoes.html*/
